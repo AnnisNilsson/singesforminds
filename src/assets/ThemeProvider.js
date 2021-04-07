8f561391ac4bdf0b24e1381c4eabedbefdca0627
josiehpersson <josephine.h.persson@outlook.com>
2021-04-07 11:04:17 +0200

    themeprovider setup

---
+++ src/assets/ThemeProvider.js
@@ -0,0 +1,16 @@
+import {createMuiTheme} from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#238584',
+            text: '#FFFFFF'
+        },
+        secondary: {
+            main: '#CE9580',
+            text: '#373636',
+        },
+    },
+});
+
+export default theme;
